91d96427f74baf8b58749011cd8eb7455458057e
Ekaterina Silchenko <maksil.17@gmail.com>
2022-10-21 23:59:46 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './css/styles.css';
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let lightbox = new SimpleLightbox('.gallery a');
